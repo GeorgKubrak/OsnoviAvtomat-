bc8c095f340e57e4f38b6a266715a2cc92833442
GeorgKubrak <118108459+georgkubrak@users.noreply.github.com>
2023-03-22 18:58:32 +0500

    Create gradle.yml

---
+++ src/test/java/service/CashbackHackServiceTest.java
@@ -0,0 +1,26 @@
+package service;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CashbackHackServiceTest {
+
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldReturnNull() {
+        int actual = service.remain(1000);
+        int expected = 0;
+        Assert.assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void shouldReturn100() {
+        int actual = service.remain(900);
+        int expected = 100;
+        Assert.assertEquals(actual, expected);
+    }
+
+
+}
